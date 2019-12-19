067173a20e2ee181e5cf6a7ad12f33d29ef25012
strizhko <strizhko_aa@it-thematic.ru>
2019-12-19 13:28:50 +0300

    try fix edge grid(thats a second try) and test meta for a detail view

--- nuxt.config.js
+++ nuxt.config.js
@@ -109,23 +109,26 @@ export default {
     //   }
     // },
 
-    /*
-    ** Run ESLINT on save
-    */
-    // extend (config, ctx) {
-    //   if (ctx.isClient) {
-    //     config.module.rules.push({
-    //       enforce: 'pre',
-    //       test: /\.(js|vue)$/,
-    //       loader: 'eslint-loader',
-    //       exclude: /(node_modules)/
-    //     })
-    //   }
-    // },
-
-    postcss: [
-      require('autoprefixer')()
-    ],
+    // postcss: [
+    //   require('autoprefixer')()
+    // ],
+    postcss: {
+      // Add plugin names as key and arguments as value
+      // Install them before as dependencies with npm or yarn
+      plugins: {
+        // Disable a plugin by passing false as value
+        'postcss-url': false,
+        // 'postcss-nested': {},
+        // 'postcss-responsive-type': {},
+        // 'postcss-hexrgba': {}
+      },
+      preset: {
+        // Change the postcss-preset-env settings
+        autoprefixer: {
+          grid: true
+        }
+      }
+    }
 
   },
   generate: {
--- pages/gallery/_id.vue
+++ pages/gallery/_id.vue
@@ -48,6 +48,39 @@ no-ssr
 	import { mapState } from 'vuex';
 
 	export default {
+		head: {
+			title: 'Alexander Babinskiy',
+			meta: [
+				{ charset: 'utf-8' },
+				{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+				{ name: 'apple-mobile-web-app-title', content: 'Babinskiy' },
+				{ name: 'application-name', content: 'Babinskiy' },
+				{ name: 'msapplication-TileColor', content: '#ffffff' },
+				{ name: 'theme-color', content: '#ffffff' },
+				{ hid: 'description', name: 'description', content: 'Artist abstract expressionism' },
+				// Open Graph / Facebook
+				{ property: 'og:type', content: 'website'},
+				{ property: 'og:url', content: 'https://babinskiy.com/'},
+				{ property: 'og:title', content: 'Alexander Babinskiy'},
+				{ property: 'og:description', content: 'Artist abstract expressionism'},
+				{ property: 'og:image', content: 'http://images.ctfassets.net/iqefgd30u0a2/2zjVYYLFpierD58gY8xGA4/80b5b8b10fb23e1f570112019a4274c7/M_-_Capture_One_Catalog7214.jpg'},
+				// Twitter
+				{ property: 'twitter:card', content: 'http://images.ctfassets.net/iqefgd30u0a2/2zjVYYLFpierD58gY8xGA4/80b5b8b10fb23e1f570112019a4274c7/M_-_Capture_One_Catalog7214.jpg'},
+				{ property: 'twitter:url', content: 'https://babinskiy.com/'},
+				{ property: 'twitter:title', content: 'Alexander Babinskiy'},
+				{ property: 'twitter:description', content: 'Artist abstract expressionism'},
+				{ property: 'twitter:image', content: 'http://images.ctfassets.net/iqefgd30u0a2/2zjVYYLFpierD58gY8xGA4/80b5b8b10fb23e1f570112019a4274c7/M_-_Capture_One_Catalog7214.jpg'},
+			],
+			link: [
+				{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+				{ rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
+				{ rel: 'icon', type: 'image/png', href: '/favicon-16x16.png' },
+				{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+				{ rel: 'manifest', href: '/site.webmanifest' },
+				{ rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e3572e' },
+				{ rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Oswald:300,400,500,600&display=swap&subset=cyrillic' }
+			],
+		},
 		async asyncData(context) {
         const contentful = context.app.contentful
 
@@ -496,20 +529,14 @@ body {
 	&__inner {
 		display: -ms-grid;
 		display: grid;
-		-ms-grid-template: "topSpace topSpace topSpace"
-									 "leftName picture rightTopSpace"
-									 "leftNav picture rightNav"
-									 "leftDesc picture rightBotSpace"
-									 "bottomSpace bottomSpace bottomSpace";
+		-ms-grid-rows: auto 0 auto 0 auto 0 auto;
 		grid-template: "topSpace topSpace topSpace"
 									 "leftName picture rightTopSpace"
 									 "leftNav picture rightNav"
 									 "leftDesc picture rightBotSpace"
 									 "bottomSpace bottomSpace bottomSpace";
-		// grid-template-columns: repeat(3, 1fr);
-		-ms-grid-template-columns: 1fr 5fr 1fr;
+		-ms-grid-columns: 1fr 5fr 1fr;
 		grid-template-columns: 1fr 5fr 1fr;
-		-ms-grid-row-gap: 0;
 		grid-row-gap: 0;
 	}
 	&__meta {
