ca10c57bd7d2dcb4e8382e3e1d90d159126c9563
strizhko <strizhko_aa@it-thematic.ru>
2019-12-30 13:15:09 +0300

    back

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,39 +17,39 @@ export default {
   /*
   ** Headers of the page
   */
-  // head: {
-  //   title: 'Alexander Babinskiy',
-  //   meta: [
-  //     { charset: 'utf-8' },
-  //     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-  //     { hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: 'Babinskiy' },
-  //     { hid: 'application-name', name: 'application-name', content: 'Babinskiy' },
-  //     { name: 'msapplication-TileColor', content: '#ffffff' },
-  //     { name: 'theme-color', content: '#ffffff' },
-  //     { hid: 'description', name: 'description', content: 'Artist abstract expressionism' },
-  //     // Open Graph / Facebook
-  //     { property: 'og:type', content: 'website'},
-  //     { hid: 'og:url', property: 'og:url', content: 'https://babinskiy.com/'},
-  //     { hid: 'og:title', property: 'og:title', content: 'Alexander Babinskiy'},
-  //     { hid: 'og:description', property: 'og:description', content: 'Artist abstract expressionism'},
-  //     { hid: 'og:image', property: 'og:image', content: 'http://images.ctfassets.net/iqefgd30u0a2/2zjVYYLFpierD58gY8xGA4/80b5b8b10fb23e1f570112019a4274c7/M_-_Capture_One_Catalog7214.jpg'},
-  //     // Twitter
-  //     { hid: 'twitter:card', property: 'twitter:card', content: 'http://images.ctfassets.net/iqefgd30u0a2/2zjVYYLFpierD58gY8xGA4/80b5b8b10fb23e1f570112019a4274c7/M_-_Capture_One_Catalog7214.jpg'},
-  //     { hid: 'twitter:url', property: 'twitter:url', content: 'https://babinskiy.com/'},
-  //     { hid: 'twitter:title', property: 'twitter:title', content: 'Alexander Babinskiy'},
-  //     { hid: 'twitter:description', property: 'twitter:description', content: 'Artist abstract expressionism'},
-  //     { hid: 'twitter:image', property: 'twitter:image', content: 'http://images.ctfassets.net/iqefgd30u0a2/2zjVYYLFpierD58gY8xGA4/80b5b8b10fb23e1f570112019a4274c7/M_-_Capture_One_Catalog7214.jpg'},
-  //   ],
-  //   link: [
-  //     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-  //     { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
-  //     { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png' },
-  //     { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-  //     { rel: 'manifest', href: '/site.webmanifest' },
-  //     { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e3572e' },
-  //     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Oswald:300,400,500,600&display=swap&subset=cyrillic' }
-  //   ],
-  // },
+  head: {
+    title: 'Alexander Babinskiy',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'apple-mobile-web-app-title', name: 'apple-mobile-web-app-title', content: 'Babinskiy' },
+      { hid: 'application-name', name: 'application-name', content: 'Babinskiy' },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'theme-color', content: '#ffffff' },
+      { hid: 'description', name: 'description', content: 'Artist abstract expressionism' },
+      // Open Graph / Facebook
+      { property: 'og:type', content: 'website'},
+      { hid: 'og:url', property: 'og:url', content: 'https://babinskiy.com/'},
+      { hid: 'og:title', property: 'og:title', content: 'Alexander Babinskiy'},
+      { hid: 'og:description', property: 'og:description', content: 'Artist abstract expressionism'},
+      { hid: 'og:image', property: 'og:image', content: 'http://images.ctfassets.net/iqefgd30u0a2/2zjVYYLFpierD58gY8xGA4/80b5b8b10fb23e1f570112019a4274c7/M_-_Capture_One_Catalog7214.jpg'},
+      // Twitter
+      { hid: 'twitter:card', property: 'twitter:card', content: 'http://images.ctfassets.net/iqefgd30u0a2/2zjVYYLFpierD58gY8xGA4/80b5b8b10fb23e1f570112019a4274c7/M_-_Capture_One_Catalog7214.jpg'},
+      { hid: 'twitter:url', property: 'twitter:url', content: 'https://babinskiy.com/'},
+      { hid: 'twitter:title', property: 'twitter:title', content: 'Alexander Babinskiy'},
+      { hid: 'twitter:description', property: 'twitter:description', content: 'Artist abstract expressionism'},
+      { hid: 'twitter:image', property: 'twitter:image', content: 'http://images.ctfassets.net/iqefgd30u0a2/2zjVYYLFpierD58gY8xGA4/80b5b8b10fb23e1f570112019a4274c7/M_-_Capture_One_Catalog7214.jpg'},
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#e3572e' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700|Oswald:300,400,500,600&display=swap&subset=cyrillic' }
+    ],
+  },
   transition: {
     name: 'page',
     mode: 'out-in'
