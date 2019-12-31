825f2c7f264974be20560bdedaf08bfa85b9dc31
Strizhko_AA <amm-andreistrizhko@yandex.ru>
2019-12-31 14:05:14 +0300

    i hope it works

--- nuxt.config.js
+++ nuxt.config.js
@@ -132,24 +132,24 @@ export default {
   },
   generate: {
     fallback: true,
-    // routes: function () {
-    //   // let axios = require('@nuxtjs/axios')
-    //   return axios.get('https://cdn.contentful.com/spaces/iqefgd30u0a2/environments/master/entries?content_type=picture&limit=999&locale=&order=fields.rating',
-    //   {headers: {
-    //     'Authorization': 'Bearer HgrYn_QMzmY3Uf8dp_BbV8b1qoXwYUuQDTeHvDqaCTk'
-    //   }})
-    //   .then(response => {
-    //     let _routes = []
-    //     // console.log(response)
-    //     for (let i in response.data.items) {
-    //       _routes.push('/gallery/' + response.data.items[i].sys.id)
-    //     }
-    //     // console.log(_routes)
-    //     return _routes
-    //   }).catch(err => {
-    //     console.log(err)
-    //   })
-    // }
+    routes: function () {
+      // let axios = require('@nuxtjs/axios')
+      return axios.get('https://cdn.contentful.com/spaces/iqefgd30u0a2/environments/master/entries?content_type=picture&limit=999&locale=&order=fields.rating',
+      {headers: {
+        'Authorization': 'Bearer HgrYn_QMzmY3Uf8dp_BbV8b1qoXwYUuQDTeHvDqaCTk'
+      }})
+      .then(response => {
+        let _routes = []
+        // console.log(response)
+        for (let i in response.data.items) {
+          _routes.push('/gallery/' + response.data.items[i].sys.id)
+        }
+        // console.log(_routes)
+        return _routes
+      }).catch(err => {
+        console.log(err)
+      })
+    }
   },
 
   /*
